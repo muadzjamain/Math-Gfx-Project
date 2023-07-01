c560a0fb25baed682edb2d5f123ff96806103edd
YAP YEE JIA <jia-03@graduate.utm.my>
2023-07-01 17:57:46 +0800

    Update Transl+Jia.cpp

--- Math GraphicProject/Transl+Jia.cpp
+++ Math GraphicProject/Transl+Jia.cpp
@@ -1,142 +1,8 @@
 #include <iostream>
-#include<conio.h>
-#include<graphics.h>
-#include<dos.h>
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
 
-int i;
-
-class stickman
-{
-	
-	public:
-		void body();
-	
-};
-
-void stickman::body() //Moving Function (Translation)
-{
-    int i = 0;
-    for(i=0;i<=300;i++)
-    {
-    // STICKMAN
-    circle(150+i, 470, 40); //head
-    line(150+i, 510, 150+i, 690); //body
-    line(150+i, 530, 50+i, 580); //arm
-    //line(150+i, 530, 250+i, 580); //arm
-    line(150+i, 690, 80+i, 750); //leg
-    //line(150+i, 690, 220+i, 750); //leg
-
-    //seller
-    circle(800, 470, 40); //head
-    line(800, 510, 800, 570); //body
-
-    line(800, 530, 700, 480);//arm
-    rectangle(700, 430, 703, 480); //batang
-    rectangle(700, 430, 775, 395); //flag
-    outtextxy(716, 405, "RM5:00");
-
-    line(800, 530, 900, 510); //arm
-    rectangle(900, 460, 903, 510); //batang
-    rectangle(900, 460, 970, 425); //flag
-    outtextxy(910, 435, "RM5:00");
-
-
-   //STALL
-    rectangle(600, 770, 1200, 570); //wall
-    rectangle(630, 570, 650, 350);
-    rectangle(1155, 570, 1175, 350);
-    rectangle(630, 330, 1175, 350);
- 
-    line(630, 330, 690, 280); //roof
-    line(1116, 280, 1176, 330);
-    line(690, 280, 1115, 280);
-
-    settextstyle(BOLD_FONT, HORIZ_DIR, 5);
-    outtextxy(630, 600, "RAHMAH");
-    outtextxy(630, 650, "5TALL");
-
-    //clouds
-    setcolor(LIGHTBLUE);
-    line(55, 100, 355, 100);
-    arc(106, 100, 360, 180, 50);
-    arc(206, 100, 360, 180, 80);
-    arc(306, 100, 360, 180, 50);
-
-    setcolor(LIGHTBLUE);
-    line(400, 150, 700, 150);
-    arc(450, 150, 360, 180, 50);
-    arc(550, 150, 360, 180, 80);
-    arc(650, 150, 360, 180, 50);
-
-    setcolor(LIGHTBLUE);
-    line(770, 100, 1070, 100);
-    arc(820, 100, 360, 180, 50);
-    arc(920, 100, 360, 180, 80);
-    arc(1020, 100, 360, 180, 50);
-
-    setcolor(LIGHTBLUE);
-    line(1170, 150, 1470, 150);
-    arc(1220, 150, 360, 180, 50);
-    arc(1320, 150, 360, 180, 80);
-    arc(1420, 150, 360, 180, 50);
-
-    //menu
-    setcolor(WHITE);
-    settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
-    outtextxy(925, 605, "SET A");
-    outtextxy(1055, 605, "SET B");
-    setcolor(WHITE);
-    rectangle(900, 600, 1150, 740);
-    line(1025, 600, 1025, 740);
-    line(900, 625, 1150, 625);
-
-    // (set A)
-    setcolor(RED);
-    rectangle(918, 675, 942, 705); //bekas
-    setcolor(LIGHTRED);
-    arc(930, 675, 360, 180, 12); //penutup
-    setcolor(WHITE);
-    rectangle(928, 650, 931, 663); //straw
-
-    //burger (set A)
-    setcolor(BROWN);
-    arc(980, 685, 360, 180, 15);
-    setcolor(BROWN);
-    rectangle(965, 685, 995, 689);
-    setcolor(GREEN);
-    rectangle(963, 690, 997, 694);
-    setcolor(BROWN);
-    rectangle(965, 695, 995, 699);
-    setcolor(BROWN);
-    rectangle(965, 700, 995, 706);
-
-    //drink (set B)
-    setcolor(RED);
-    rectangle(1045, 675, 1069, 705); //bekas
-    setcolor(LIGHTRED);
-    arc(1057, 675, 360, 180, 12); //penutup
-    setcolor(WHITE);
-    rectangle(1055, 650, 1058, 663); //straw
-
-    //donut(set B)
-    setcolor(BROWN); //outer
-    circle(1100, 680, 20);
-    setcolor(WHITE); //inner
-    circle(1100, 680, 8);
-   
-    setcolor(YELLOW);
-    circle(1120, 695, 20);
-    setcolor(WHITE);
-    circle(1120, 695, 8);
-    
-
-    delay(10);
-    cleardevice();
-
-    }
-}
-
-// Ye Jia FX Down Here
 bool isMouseClicked(int mx, int my, int x[], int y[])
 {
 
@@ -194,48 +60,57 @@ void shearing(int c[], int d[], float shx, int n)
     }
 }
 
-int main()
+void moving() // Moving Function (Translation)
 {
-    initwindow(getmaxwidth(), getmaxheight(), "Menu Rahmah");
+    int h[] = {150};
+    int g[] = {470};
+    int s[] = {150, 150};
+    int e[] = {510, 690};
+    int p[] = {150, 50};
+    int q[] = {530, 580};
+    int u[] = {150, 80};
+    int t[] = {690, 750};
 
-	int gm,i,a;
-	initwindow(1700,800);
-
-    // Set initial position of stickman
-
-    stickman c;
-	c.body();
-
-/////*  Static Stickman, stall, Cloud & etc DOWN HERE  *//////
-
-char ch = 0;
-
-while (ch != 27) // 27 is ESC key
+    for (int i = 0; i <= 300; i++)
     {
-
-    // STICKMAN
-    circle(150+300, 470, 40); //head
-    line(150+300, 510, 150+300, 690); //body
-    line(150+300, 530, 50+300, 580); //arm
-    //line(150+300, 530, 250+300, 580); //arm
-    line(150+300, 690, 80+300, 750); //leg
-    //line(150+300, 690, 220+300, 750); //leg
+        // STICKMAN
+        setcolor(WHITE);
+        translation(h, g, 1, 0, 1);
+        circle(h[0], g[0], 40); // head
+        translation(s, e, 1, 0, 2);
+        line(s[0], e[0], s[0], e[1]); // body
+        translation(p, q, 1, 0, 2);
+        line(p[0], q[0], p[1], q[1]); // arm
+        // line(150+i, 530, 250+i, 580); //arm
+        translation(u, t, 1, 0, 2);
+        line(u[0], t[0], u[1], t[1]); // leg
+        // line(150+i, 690, 220+i, 750); //leg
 
         // seller
         circle(800, 470, 40);     // head
         line(800, 510, 800, 570); // body
 
-        line(800, 530, 700, 480);      // arm
+        setcolor(WHITE);
+        line(800, 530, 700, 480); // arm
+        setcolor(RED);
         rectangle(700, 430, 703, 480); // batang
+        setcolor(GREEN);
         rectangle(700, 430, 775, 395); // flag
-        outtextxy(716, 405, "RM5:00");
+        settextstyle(BOLD_FONT, HORIZ_DIR, 1);
+        char word1[] = {'R', 'M', '5', '.', '0', '0', '\0'};
+        outtextxy(705, 405, word1);
 
-        line(800, 530, 900, 510);      // arm
+        setcolor(WHITE);
+        line(800, 530, 900, 510); // arm
+        setcolor(RED);
         rectangle(900, 460, 903, 510); // batang
+        setcolor(GREEN);
         rectangle(900, 460, 970, 425); // flag
-        outtextxy(910, 435, "RM5:00");
+        settextstyle(BOLD_FONT, HORIZ_DIR, 1);
+        outtextxy(903, 435, word1);
 
         // STALL
+        setcolor(BROWN);
         rectangle(600, 770, 1200, 570); // wall
         rectangle(630, 570, 650, 350);
         rectangle(1155, 570, 1175, 350);
@@ -245,9 +120,12 @@ while (ch != 27) // 27 is ESC key
         line(1116, 280, 1176, 330);
         line(690, 280, 1115, 280);
 
+        setcolor(WHITE);
         settextstyle(BOLD_FONT, HORIZ_DIR, 5);
-        outtextxy(630, 600, "RAHMAH");
-        outtextxy(630, 650, "5TALL");
+        char word2[] = {'R', 'A', 'H', 'M', 'A', 'H', '\0'};
+        char word3[] = {'5', 'T', 'A', 'L', 'L', '\0'};
+        outtextxy(630, 600, word2);
+        outtextxy(630, 650, word3);
 
         // clouds
         setcolor(LIGHTBLUE);
@@ -277,8 +155,164 @@ while (ch != 27) // 27 is ESC key
         // menu
         setcolor(WHITE);
         settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
-        outtextxy(925, 605, "SET A");
-        outtextxy(1055, 605, "SET B");
+        char word4[] = {'S', 'E', 'T', ' ', 'A', '\0'};
+        char word5[] = {'S', 'E', 'T', ' ', 'B', '\0'};
+        outtextxy(925, 605, word4);
+        outtextxy(1055, 605, word5);
+        setcolor(WHITE);
+        rectangle(900, 600, 1150, 740);
+        line(1025, 600, 1025, 740);
+        line(900, 625, 1150, 625);
+
+        // (set A)
+        setcolor(RED);
+        rectangle(918, 675, 942, 705); // bekas
+        setcolor(LIGHTRED);
+        arc(930, 675, 360, 180, 12); // penutup
+        setcolor(WHITE);
+        rectangle(928, 650, 931, 663); // straw
+
+        // burger (set A)
+        setcolor(BROWN);
+        arc(980, 685, 360, 180, 15);
+        setcolor(BROWN);
+        rectangle(965, 685, 995, 689);
+        setcolor(GREEN);
+        rectangle(963, 690, 997, 694);
+        setcolor(BROWN);
+        rectangle(965, 695, 995, 699);
+        setcolor(BROWN);
+        rectangle(965, 700, 995, 706);
+
+        // drink (set B)
+        setcolor(RED);
+        rectangle(1045, 675, 1069, 705); // bekas
+        setcolor(LIGHTRED);
+        arc(1057, 675, 360, 180, 12); // penutup
+        setcolor(WHITE);
+        rectangle(1055, 650, 1058, 663); // straw
+
+        // donut(set B)
+        setcolor(BROWN); // outer
+        circle(1100, 680, 20);
+        setcolor(WHITE); // inner
+        circle(1100, 680, 8);
+
+        setcolor(YELLOW);
+        circle(1120, 695, 20);
+        setcolor(WHITE);
+        circle(1120, 695, 8);
+
+        delay(10);
+        cleardevice();
+    }
+}
+
+int main()
+{
+    initwindow(getmaxwidth(), getmaxheight(), "Menu Rahmah");
+
+    int gm, i, a;
+    initwindow(1700, 800);
+
+    // Set initial position of stickman
+
+    moving();
+
+    ////*  Static Stickman, stall, Cloud & etc DOWN HERE  *//////
+
+    char ch = 0;
+
+    while (ch != 27) // 27 is ESC key
+    {
+
+        // STICKMAN
+        circle(150 + 300, 470, 40);           // head
+        line(150 + 300, 510, 150 + 300, 690); // body
+        line(150 + 300, 530, 50 + 300, 580);  // arm
+        // line(150+300, 530, 250+300, 580); //arm
+        line(150 + 300, 690, 80 + 300, 750); // leg
+        // line(150+300, 690, 220+300, 750); //leg
+
+        // seller
+        circle(800, 470, 40);     // head
+        line(800, 510, 800, 570); // body
+
+        setcolor(WHITE);
+        line(800, 530, 700, 480); // arm
+        setcolor(RED);
+        rectangle(700, 430, 703, 480); // batang
+        setcolor(GREEN);
+        rectangle(700, 430, 775, 395); // flag
+        settextstyle(BOLD_FONT, HORIZ_DIR, 1);
+        char word1[] = {'R', 'M', '5', '.', '0', '0', '\0'};
+        outtextxy(705, 405, word1);
+
+        setcolor(WHITE);
+        line(800, 530, 900, 510); // arm
+        setcolor(RED);
+        rectangle(900, 460, 903, 510); // batang
+        setcolor(GREEN);
+        rectangle(900, 460, 970, 425); // flag
+        settextstyle(BOLD_FONT, HORIZ_DIR, 1);
+        outtextxy(903, 435, word1);
+
+        // STALL
+        setcolor(YELLOW);
+        rectangle(600, 770, 1200, 570); // wall
+        rectangle(630, 570, 650, 350);
+        rectangle(1155, 570, 1175, 350);
+        rectangle(630, 330, 1175, 350);
+
+        setcolor(YELLOW);
+        line(630, 330, 690, 280); // roof
+        line(1116, 280, 1176, 330);
+        line(690, 280, 1115, 280);
+
+        setcolor(YELLOW);
+        settextstyle(BOLD_FONT, HORIZ_DIR, 4);
+        char word7[] = {'C', 'l', 'i', 'c', 'k', ' ', 'o', 'n', ' ', 't', 'h', 'e', ' ', 'm', 'e', 'n', 'u', ' ', 't', 'o', ' ', 'c', 'h', 'e', 'c', 'k', ' ', 'o', 'u', 't', ' ', 'w', 'h', 'a', 't', ' ', 'w', 'e', ' ', 'o', 'f', 'f', 'e', 'r', '!', '\0'};
+        outtextxy(450, 190, word7);
+
+        setcolor(WHITE);
+        settextstyle(BOLD_FONT, HORIZ_DIR, 5);
+        char word2[] = {'R', 'A', 'H', 'M', 'A', 'H', '\0'};
+        char word3[] = {'5', 'T', 'A', 'L', 'L', '\0'};
+        outtextxy(630, 600, word2);
+        outtextxy(630, 650, word3);
+
+        // clouds
+        setcolor(LIGHTBLUE);
+        line(55, 100, 355, 100);
+        arc(106, 100, 360, 180, 50);
+        arc(206, 100, 360, 180, 80);
+        arc(306, 100, 360, 180, 50);
+
+        setcolor(LIGHTBLUE);
+        line(400, 150, 700, 150);
+        arc(450, 150, 360, 180, 50);
+        arc(550, 150, 360, 180, 80);
+        arc(650, 150, 360, 180, 50);
+
+        setcolor(LIGHTBLUE);
+        line(770, 100, 1070, 100);
+        arc(820, 100, 360, 180, 50);
+        arc(920, 100, 360, 180, 80);
+        arc(1020, 100, 360, 180, 50);
+
+        setcolor(LIGHTBLUE);
+        line(1170, 150, 1470, 150);
+        arc(1220, 150, 360, 180, 50);
+        arc(1320, 150, 360, 180, 80);
+        arc(1420, 150, 360, 180, 50);
+
+        // menu
+        setcolor(WHITE);
+        settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+        char word4[] = {'S', 'E', 'T', ' ', 'A', '\0'};
+        char word5[] = {'S', 'E', 'T', ' ', 'B', '\0'};
+        outtextxy(925, 605, word4);
+        outtextxy(1055, 605, word5);
         setcolor(WHITE);
         // rectangle(900, 600, 1150, 740);
         line(1025, 600, 1025, 740);
@@ -344,10 +378,12 @@ while (ch != 27) // 27 is ESC key
                 // Enlarged menu
                 setcolor(WHITE);
                 settextstyle(DEFAULT_FONT, HORIZ_DIR, 6);
-                char word1[] = {'S', 'E', 'T', ' ', 'A', '\0'};
-                char word2[] = {'S', 'E', 'T', ' ', 'B', '\0'};
-                outtextxy(460, 215, word1);
-                outtextxy(855, 215, word2);
+                outtextxy(460, 215, word4);
+                outtextxy(855, 215, word5);
+                settextstyle(5, HORIZ_DIR, 5);
+                char word6[] = {'R', 'M', '5', ' ', 'O', 'n', 'l', 'y', '!', '!', '\0'};
+                outtextxy(500, 300, word6);
+                outtextxy(880, 300, word6);
                 setcolor(WHITE);
                 rectangle(x[0], y[0], x[1], y[1]);
                 line(775, 200, 775, 620);
